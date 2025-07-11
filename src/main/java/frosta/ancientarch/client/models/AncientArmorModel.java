20e40d081b429008355cd3045a4f9e1b1217f85a
Loqor <isaiahcrace@gmail.com>
2025-07-10 22:08:44 -0500

    more armor edits

--- src/main/java/frosta/ancientarch/client/models/AncientArmorModel.java
+++ src/main/java/frosta/ancientarch/client/models/AncientArmorModel.java
@@ -32,22 +32,22 @@ public class AncientArmorModel extends EntityModel<LivingEntity> {
 		ModelPartData modelPartData = modelData.getRoot();
 		ModelPartData root = modelPartData.addChild("root", ModelPartBuilder.create(), ModelTransform.pivot(0.0F, 24.0F, 0.0F));
 
-		ModelPartData head = root.addChild("head", ModelPartBuilder.create().uv(0, 0).cuboid(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, new Dilation(0.5F)), ModelTransform.pivot(0.0F, -24.0F, 0.0F));
+		ModelPartData head = root.addChild("head", ModelPartBuilder.create().uv(0, 0).cuboid(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, new Dilation(1.0F)), ModelTransform.pivot(0.0F, -24.0F, 0.0F));
 
 		ModelPartData horns = head.addChild("horns", ModelPartBuilder.create(), ModelTransform.of(0.0F, -8.5329F, -0.7627F, 1.4399F, 0.0F, 0.0F));
 
-		ModelPartData cube_r1 = horns.addChild("cube_r1", ModelPartBuilder.create().uv(0, 0).cuboid(-1.0F, -5.0F, -1.0F, 2.0F, 5.0F, 2.0F, new Dilation(-0.1F)), ModelTransform.of(4.0463F, 0.0682F, -0.0625F, -0.7182F, -0.2046F, -0.2284F));
+		ModelPartData cube_r1 = horns.addChild("cube_r1", ModelPartBuilder.create().uv(0, 0).cuboid(-1.0F, -5.0F, -1.0F, 2.0F, 5.0F, 2.0F, new Dilation(-0.1F)), ModelTransform.of(5.0463F, 0.0682F, -0.0625F, -0.7182F, -0.2046F, -0.2284F));
 
 		ModelPartData cube_r2 = horns.addChild("cube_r2", ModelPartBuilder.create().uv(0, 0).mirrored().cuboid(-1.0F, -5.0F, -1.0F, 2.0F, 5.0F, 2.0F, new Dilation(0.0F)).mirrored(false)
-				.uv(0, 0).cuboid(-9.0F, -5.0F, -1.0F, 2.0F, 5.0F, 2.0F, new Dilation(0.0F)), ModelTransform.of(4.0F, 3.5329F, -3.2373F, -0.7418F, 0.0F, 0.0F));
+				.uv(0, 0).cuboid(-11.0F, -5.0F, -1.0F, 2.0F, 5.0F, 2.0F, new Dilation(0.0F)), ModelTransform.of(5.0F, 3.5329F, -3.2373F, -0.7418F, 0.0F, 0.0F));
 
-		ModelPartData cube_r3 = horns.addChild("cube_r3", ModelPartBuilder.create().uv(0, 0).cuboid(-1.0F, -5.0F, -1.0F, 2.0F, 5.0F, 2.0F, new Dilation(-0.1F)), ModelTransform.of(-4.0463F, 0.0682F, -0.0625F, -0.7182F, 0.2046F, 0.2284F));
+		ModelPartData cube_r3 = horns.addChild("cube_r3", ModelPartBuilder.create().uv(0, 0).cuboid(-1.0F, -5.0F, -1.0F, 2.0F, 5.0F, 2.0F, new Dilation(-0.1F)), ModelTransform.of(-5.0463F, 0.0682F, -0.0625F, -0.7182F, 0.2046F, 0.2284F));
 
-		ModelPartData body = root.addChild("body", ModelPartBuilder.create().uv(16, 16).cuboid(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, new Dilation(0.5F)), ModelTransform.pivot(0.0F, -24.0F, 0.0F));
+		ModelPartData body = root.addChild("body", ModelPartBuilder.create().uv(16, 16).cuboid(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, new Dilation(1.0F)), ModelTransform.pivot(0.0F, -24.0F, 0.0F));
 
-		ModelPartData rightArm = root.addChild("rightArm", ModelPartBuilder.create().uv(40, 16).cuboid(-3.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(1.0F)), ModelTransform.pivot(-5.0F, -22.0F, 0.0F));
+		ModelPartData rightArm = root.addChild("rightArm", ModelPartBuilder.create().uv(40, 16).cuboid(-3.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(1.25F)), ModelTransform.pivot(-5.0F, -22.0F, 0.0F));
 
-		ModelPartData leftArm = root.addChild("leftArm", ModelPartBuilder.create().uv(40, 16).mirrored().cuboid(-1.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(1.0F)).mirrored(false), ModelTransform.pivot(5.0F, -22.0F, 0.0F));
+		ModelPartData leftArm = root.addChild("leftArm", ModelPartBuilder.create().uv(40, 16).mirrored().cuboid(-1.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(1.25F)).mirrored(false), ModelTransform.pivot(5.0F, -22.0F, 0.0F));
 
 		ModelPartData rightLeg = root.addChild("rightLeg", ModelPartBuilder.create().uv(0, 16).cuboid(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new Dilation(0.5F)), ModelTransform.pivot(-1.9F, -12.0F, 0.0F));
 
