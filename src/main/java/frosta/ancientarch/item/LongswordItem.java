4bff8a0ce957e8089f3fc34da91322d06e99b6a1
Loqor <isaiahcrace@gmail.com>
2025-07-15 17:38:01 -0500

    rahh

--- src/main/java/frosta/ancientarch/item/LongswordItem.java
+++ src/main/java/frosta/ancientarch/item/LongswordItem.java
@@ -1,10 +1,51 @@
 package frosta.ancientarch.item;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterial;
+import net.minecraft.world.World;
 
 public class LongswordItem extends SwordItem {
+    private static final int BUFF_INTERVAL_TICKS = 20; // 1 second (20 ticks)
+    private static final float BUFFED_ATTACK_DAMAGE = 12.5f; // example buffed value
+    private static final float NORMAL_ATTACK_DAMAGE = 9.5f; // example normal value
+
+    private int tickCounter = 0;
+    private float currentAttackDamage;
+
     public LongswordItem(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
         super(toolMaterial, attackDamage, attackSpeed, settings);
+        this.currentAttackDamage = attackDamage;
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+        if (entity instanceof PlayerEntity player && !world.isClient) {
+            tickCounter++;
+            if (tickCounter >= BUFF_INTERVAL_TICKS) {
+                tickCounter = 0;
+                if (hasFullSet(player)) {
+                    currentAttackDamage = BUFFED_ATTACK_DAMAGE;
+                } else {
+                    currentAttackDamage = NORMAL_ATTACK_DAMAGE;
+                }
+            }
+        }
+        super.inventoryTick(stack, world, entity, slot, selected);
+    }
+
+    @Override
+    public float getAttackDamage() {
+        return currentAttackDamage;
+    }
+
+    public static boolean hasFullSet(PlayerEntity player) {
+        return player.getEquippedStack(EquipmentSlot.HEAD).getItem() instanceof AncientArmorItem
+                && player.getEquippedStack(EquipmentSlot.CHEST).getItem() instanceof AncientArmorItem
+                && player.getEquippedStack(EquipmentSlot.LEGS).getItem() instanceof AncientArmorItem
+                && player.getEquippedStack(EquipmentSlot.FEET).getItem() instanceof AncientArmorItem;
     }
 }
