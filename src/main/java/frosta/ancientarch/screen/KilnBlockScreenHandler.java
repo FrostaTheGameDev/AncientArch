154bd8db5dae20848299e7981f8c4375fed3b49b
Nightedmare <nighted.jester@gmail.com>
2025-07-25 08:05:59 +0200

    Gugu Gaga

--- src/main/java/frosta/ancientarch/screen/KilnBlockScreenHandler.java
+++ src/main/java/frosta/ancientarch/screen/KilnBlockScreenHandler.java
@@ -31,9 +31,9 @@ public class KilnBlockScreenHandler extends ScreenHandler {
         this.propertyDelegate = arrayPropertyDelegate;
         this.blockEntity = ((KilnBlockEntity) blockEntity);
 
-        this.addSlot(new Slot(inventory, 0, 116, 11));
+        this.addSlot(new Slot(inventory, 0, 50, 11));
         this.addSlot(new Slot(inventory, 1, 80, 11));
-        this.addSlot(new Slot(inventory, 2, 50, 11));
+        this.addSlot(new Slot(inventory, 2, 116, 11));
         this.addSlot(new Slot(inventory, 3, 80, 59));
 
 
